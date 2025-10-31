f426949f99ae274bdb0c3cd8a8dc142c510e0ae2
Joaquin120 <joaquinchiarello28@gmail.com>
2025-10-31 01:01:02 +0100

    Create tp_integrador.py

---
+++ tp_integrador.py
@@ -0,0 +1,9 @@
+paises=[]
+argentina={"Nombre":"Argentina","Poblacion":46994384,"Superficie":2780400,"Continente":"América del Sur"}
+Brasil={"Nombre":"Brasil","Poblacion":220051512 ,"Superficie":8515770,"Continente":"América del Sur"}
+Japon={"Nombre":"Japon","Poblacion":123201945,"Superficie":377915,"Continente":"Asia oriental"}   #"Nombre":,"Poblacion":,"Superficie":,"Continente":
+Francia={"Nombre":"Francia","Poblacion":68374591,"Superficie":643801,"Continente":"Europa occidental"}
+Senegal={"Nombre":"Senegal","Poblacion":18847519,"Superficie":196722,"Continente":"África occidental"}
+cuba={"Nombre":"Cuba","Poblacion":10966038,"Superficie":110860,"Continente":"Centroamérica y el Caribe"}
+España={"Nombre":"España","Poblacion":47280433,"Superficie":47280433,"Continente":"suroeste de Europa"}
+China={"Nombre":"China","Poblacion":1416043270,"Superficie":9596960,"Continente":"Asia oriental"}
